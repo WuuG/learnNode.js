c7daa5b7da45d652dccf86690a441bc802f7e32d
Wuu-Server <1249732457@qq.com>
2021-05-22 14:09:38 +0800

    🐛 复习，更新笔记错误

--- 01-videoCode/01-nodeBasics/02-tools/01-npm/1.script/script1.js
+++ 01-videoCode/01-nodeBasics/02-tools/01-npm/1.script/script1.js
@@ -1,4 +1,5 @@
 console.log("这是第一个脚本文件");
 
 console.log(process.env.npm_package_version);
-console.log(process.env.npm_package_scripts);
+console.log(process.env.npm_package_scripts_start);
+console.log(process.env.npm_package_config_env);
