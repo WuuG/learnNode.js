0d9c3191a8219678b3c9ecc6d42e29895afdf1d1
Wuu-Server <1249732457@qq.com>
2021-05-30 15:48:04 +0800

    📱 登录窗口响应式设计

--- 01-videoCode/lagou-admin/vue-frontend/src/views/passport/Passport.vue
+++ 01-videoCode/lagou-admin/vue-frontend/src/views/passport/Passport.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
-    <h1>这是passport</h1>
-    <router-view></router-view>
+  <div class="background">
+    <el-row class="header"></el-row>
+    <el-row class="content">
+      <el-col :xs="12" :sm="10" :md="9" :lg="8" :xl="7" class="content-box">
+        <router-view></router-view>
+      </el-col>
+    </el-row>
+    <el-row class="footer"></el-row>
   </div>
 </template>
 
@@ -10,4 +15,28 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+.background {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  .content {
+    flex: 5;
+    height: auto;
+    min-height: 500px;
+    .content-box {
+      float: none;
+      margin: 0 auto;
+      background-color: #eee;
+    }
+  }
+  &:before {
+    content: "";
+    flex: 2;
+  }
+  &:after {
+    content: "";
+    flex: 2;
+  }
+}
 </style>
